6c7d00372ab3e1727900b27d0bc75bfebcb6409b
Steven Bowtie <steven.bowtie@gmail.com>
2020-03-23 15:01:29 -0700

    Populate config page from JSON string

--- serverConfig.h
+++ serverConfig.h
@@ -52,7 +52,6 @@ void init_server_callbacks(){
     request->send(200, "text/plain", "Configuration \n" + message);
   });
 
-  // Send a GET request to <IP>/get?message=<message>
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
       String message;
       if (request->hasParam(PARAM_MESSAGE)) {
@@ -63,29 +62,22 @@ void init_server_callbacks(){
       request->send(200, "text/plain", "Hello, GET: " + message);
   });
 
-  // Send a POST request to <IP>/post with a form field message set to <message>
   server.on("/spiffs", HTTP_GET, [](AsyncWebServerRequest *request){
-      String message;
+    /*  String message;
       File file = SPIFFS.open("/spiffs.htm");
       if(!file){ message="Failed to open file for reading"; }
 
       while(file.available()){
         message.concat( file.read() );
       }
-      file.close();
+      file.close(); */
       request->send(SPIFFS,"/spiffs.htm", "text/html");
   });
 
-  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
-      String message;
-      if (request->hasParam(PARAM_MESSAGE, true)) {
-          message = request->getParam(PARAM_MESSAGE, true)->value();
-      } else {
-          message = "No message sent";
-      }
-      request->send(200, "text/plain", "Hello, POST: " + message);
+  server.on("/config.json", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(SPIFFS,"/config.json", "text/html");
   });
-  
+
   server.onNotFound(notFound);
   Serial.println("Server Configured");
 }
